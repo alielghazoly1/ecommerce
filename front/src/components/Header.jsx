9e9f6229035f3e2792fcfad7f978f04fbeea64fc
alielghazoly1 <alielghazoly769@gmail.com>
2025-12-12 00:14:33 +0200

    solved bug in menuItems componant

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import MenuItems from './MenuItems';
-
 import { Rocket, Menu, X } from 'lucide-react';
 
 const Header = () => {
@@ -15,8 +14,9 @@ const Header = () => {
     handleResize();
     return () => window.removeEventListener('resize', handleResize);
   }, []);
-return (
+  return (
     <>
+      {/*  header desktop */}
       <header className="hidden h-12 md:flex items-center px-10 py-10 w-full fixed top-0 left-0 bg-linear-to-r from-indigo-800 via-purple-800 to-pink-800 backdrop-blur-md shadow-xl z-50 ">
         <div className="items-center gap-4 lg:flex hidden">
           <Rocket className="w-8 h-8 text-cyan-400 animate-pulse" />
@@ -28,6 +28,7 @@ return (
           <MenuItems isMobile={false} />
         </div>
       </header>
+      {/* header mobile */}
       <header className="md:hidden h-12 flex justify-between items-center px-4 py-4 w-full fixed top-0 bg-linear-to-r from-indigo-900 via-purple-900 to-pink-900 backdrop-blur-md shadow-xl z-50">
         <div className="flex items-center gap-2">
           <Rocket className="w-8 h-8 text-cyan-400 animate-pulse" />
@@ -44,32 +45,34 @@ return (
           <Menu className="w-6 h-8 cursor-pointer" />
         </button>
       </header>
-      {/* sidebar Mobile */}
+      {/* sidebar mobile */}
       <aside
         className={`fixed top-12 right-0 h-full w-72 bg-linear-to-b from-indigo-900 via-purple-900 to-pink-900 shadow-2xl backdrop-blur-md transform transition-transform duration-500 z-40 ${
           sidebarOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
-        <div className='flex justify-end p-4'>
-           <button onClick={()=>{
-             setSidebarOpen(false);
-           }} className='text-white p-3 hover:bg-white/20 rounded-lg transition-all duration-300'>
-            <X className='w-7 h-7'/>
-
-           </button>
+        <div className="flex justify-end p-4">
+          <button
+            onClick={() => {
+              setSidebarOpen(false);
+            }}
+            className="text-white p-3 hover:bg-white/20 rounded-lg transition-all duration-300"
+          >
+            <X className="w-7 h-7" />
+          </button>
         </div>
-        <div className='mt-6 px-6 space-y-6'>
+        <div className="mt-6 px-6 space-y-6">
           <MenuItems setSidebarOpen={setSidebarOpen} isMobile={true} />
-
         </div>
       </aside>
-      {
-        sidebarOpen && (
-          <div className='sm:hidden fixed inset-0 bg-black/60 backdrop-blur-sm z-30'
-          onClick={()=>{setSidebarOpen(false)}}
-          ></div>
-        )
-      }
+      {sidebarOpen && (
+        <div
+          className="sm:hidden fixed inset-0 bg-black/60 backdrop-blur-sm z-30"
+          onClick={() => {
+            setSidebarOpen(false);
+          }}
+        ></div>
+      )}
     </>
   );
 };
--- front/src/components/MenuItems.jsx
+++ front/src/components/MenuItems.jsx
@@ -1,54 +1,56 @@
-import React from 'react';
-import {
-  Home,
-  FolderOpen,
-  ShoppingBag,
-  Mail,
-  ShoppingCart,
-  User,
-} from 'lucide-react';
-import { useState, useContext } from 'react';
+import React, { useState } from 'react';
+import { Home, FolderOpen, ShoppingBag, Mail, User } from 'lucide-react';
 import { Link as ScrollLink } from 'react-scroll';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export const menuItemData = [
-  { to: 'home', label: 'Home', icon: Home },
-  { to: 'categories', label: 'Categories', icon: FolderOpen },
-  { to: 'shopping', label: 'Shop', icon: ShoppingBag },
-  { to: 'contact', label: 'Contact', icon: Mail },
+  { to: 'home', label: 'Home', Icon: Home },
+  { to: 'categories', label: 'Categories', Icon: FolderOpen },
+  { to: 'shopping', label: 'Shop', Icon: ShoppingBag },
+  { to: 'contact', label: 'Contact', Icon: Mail },
 ];
+
 const MenuItems = ({ setSideBarOpen, isMobile }) => {
+  const [cartItems] = useState({ item1: 2, item2: 1 });
+  const [token, setToken] = useState(true);
+  const location = useLocation();
+  const navigate = useNavigate();
+
   const totalItems = Object.values(cartItems).reduce((a, b) => a + b, 0);
+
   const handeleLogout = () => {
     localStorage.removeItem('token');
     setToken(false);
     navigate('/');
     setSideBarOpen && setSideBarOpen(false);
   };
-  const navigate = useNavigate();
+
   return (
     <div
       className={`flex md:justify-center lg:justify-end ${
         isMobile
-          ? 'flex space-y-6 items-center px-4 gap-y-2'
+          ? 'flex-col space-y-6 items-center px-4 gap-y-2'
           : 'flex-row w-full items-center gap-4'
       }`}
     >
       {menuItemData.map(({ to, label, Icon }) => {
-        window.location.pathname === '/' ? (
+        return location.pathname === '/' ? (
           <ScrollLink
             key={to}
             to={to}
             smooth={true}
             duration={500}
-            offset={-70}
+            offset={-80}
             spy={true}
             onClick={() => {
               setSideBarOpen && setSideBarOpen(false);
             }}
-            className="flex items-center gap-3 px-4 py-3 rounded-lg h-[35px] transition-all shrink-0 w-auto min-w-20 text-gray-200 hover:bg-white/10 hover:text-white
-            hover:shadow-md cursor-pointer"
-            acriveClass="bg-gradient-to-r from-indigo-500 to-pink-500 text-white shadow-lg"
-          ></ScrollLink>
+            className="flex items-center gap-3 px-4 py-3 rounded-lg h-[35px] transition-all shrink-0 w-auto min-w-20 text-gray-200 hover:bg-white/10 hover:text-white hover:shadow-md cursor-pointer"
+            activeClass="bg-gradient-to-r from-indigo-500 to-pink-500 text-white shadow-lg"
+          >
+            <Icon className="w-6 h-6" />
+            <span className="font-semibold text-base">{label}</span>
+          </ScrollLink>
         ) : (
           <button
             key={to}
@@ -63,13 +65,43 @@ const MenuItems = ({ setSideBarOpen, isMobile }) => {
           </button>
         );
       })}
+
       <button
         onClick={() => {
           navigate('/cart');
           setSideBarOpen && setSideBarOpen(false);
         }}
-        className=" relative flex items-center gap-3 px-4 py-3 rounded-lg h-[35px] transition-all  text-gray-200 hover:bg-white/10 hover:shadow-md "
-      ></button>
+        className="relative flex items-center gap-3 px-4 py-3 rounded-lg h-[35px] transition-all text-gray-200 hover:bg-white/10 hover:shadow-md "
+      >
+        <ShoppingBag className="w-6 h-6" />
+        {totalItems > 0 && (
+          <span className="absolute -top-1 -right-1 w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full flex items-center justify-center">
+            {totalItems}
+          </span>
+        )}
+      </button>
+
+      {!token ? (
+        <button
+          onClick={() => {
+            navigate('/login');
+            setSideBarOpen && setSideBarOpen(false);
+          }}
+          className="flex items-center gap-2 px-4 py-3 rounded-lg h-[35px] bg-cyan-400 text-white font-semibold hover:bg-cyan-500 transition-all "
+        >
+          Login
+        </button>
+      ) : (
+        <div className="flex items-center gap-4 ">
+          <User className="w-6 h-6" />
+          <button
+            onClick={handeleLogout}
+            className="flex items-center gap-2 px-4 py-3 rounded-lg h-[35px] bg-red-500 text-white font-semibold hover:bg-red-600 transition-all"
+          >
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 };
