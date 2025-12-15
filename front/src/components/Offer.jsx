03e68c15acea407a7fc0be1cc1fad1c9f9f19580
alielghazoly1 <alielghazoly769@gmail.com>
2025-12-15 22:30:25 +0200

    Create the offer component and use a countdown timer effect using Date()

--- front/src/components/Offer.jsx
+++ front/src/components/Offer.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { all_products } from '../assets/data';
+import { ShoppingBag } from 'lucide-react';
+import { ShopContext } from '../context/ShopContext';
 
 const Offer = () => {
-  return (
-    <div>
-      Offer
-    </div>
-  )
-}
+  const { addToCart } = useContext(ShopContext);
+  const [timeLeft, setTimeLeft] = useState({});
+  const [product, setProducts] = useState(all_products.slice(0, 12));
+  useEffect(() => {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 5);
+    const interval = setInterval(() => {
+      const now = new Date();
+      const diff = targetDate - now;
 
-export default Offer
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / 1000 / 60) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
+      setTimeLeft({days, hours, minutes, seconds})
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  console.log(timeLeft)
+  return <div>Off</div>;
+};
+
+export default Offer;
