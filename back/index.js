1ea14d33be9c7b64c69161079a45ae6c8ab9849b
alielghazoly1 <alielghazoly769@gmail.com>
2026-01-14 04:10:17 +0200

    ubdate back

--- back/index.js
+++ back/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import compression from 'compression';
 import connectDB from './config/db.js';
 import 'dotenv/config.js';
 import userRouter from './routes/userRoutes.js';
@@ -9,42 +10,32 @@ import cartRouter from './routes/cartRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-const PORT = 4000;
-
+// Middlewares
 app.use(express.json());
-
-// const allowedOrigins = ['http://localhost:5174'];
-
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
-
+app.use(express.urlencoded({ extended: true }));
+app.use(compression());
 app.use(cors());
 
-
+// Routes
 app.use('/images', express.static('uploads'));
 app.use('/api/user', userRouter);
 app.use('/api/order', orderRouter);
 app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/admin', adminRouter);
-app.use('/api/users', userRouter); // Added this line to fix the route issue
 
-app.get('/', (req, res) => {
-  res.send('API working');
-});
+// Root route
+app.get('/', (req, res) => res.send('API working'));
 
+// Start server
 app.listen(PORT, async () => {
-  connectDB();
-  console.log(`Server is running ${PORT}`);
+  try {
+    await connectDB();
+    console.log(`Server is running on port ${PORT}`);
+  } catch (err) {
+    console.error('DB connection failed', err);
+    process.exit(1);
+  }
 });
