235970f1e5da4b1110c1057390370beaf15b857f
alielghazoly1 <alielghazoly769@gmail.com>
2026-01-16 03:20:24 +0200

    Update vercel.json configuration

--- front/src/context/ShopContext.jsx
+++ front/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const [token, setToken] = useState(null);
   const [authLoading, setAuthLoading] = useState(true); // ✅ new
-  const url = 'https://ecommerce-d4caul1f6-alielghazoly1s-projects.vercel.app';
+  const url = 'https://ecommerce-nine-theta-34.vercel.app';
 
   // تحميل السلة من LocalStorage
   useEffect(() => {
