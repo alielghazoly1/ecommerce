ff05a750d2ee95e2b70391ca99d521e68af974ff
alielghazoly1 <alielghazoly769@gmail.com>
2026-01-29 03:26:20 +0200

    update

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
+  // base: 'ecommerce',
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0', // يسمح لأي جهاز في الشبكة يدخل
-    port: 5174, // البورت اللي عايز تفتح عليه
-    strictPort: true, // لو البورت مستخدم هيطلع خطأ بدل ما يغيره
-  },
 });
